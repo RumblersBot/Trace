1947230f262656b3f8bcd2f42e511e96b743f7bd
MagnoBE <blank@mail.ext>
2022-07-25 12:06:37 +0200

    permlevel check on other users

--- src/commands/server/permlevel.js
+++ src/commands/server/permlevel.js
@@ -4,8 +4,9 @@ module.exports = {
     name: "permlevel",
     category: "server",
     description: "Show your permission level within this server",
-    run: async ({ message }) => {
-        let target
+    usage: "[User]",
+    run: async ({ message, args }) => {
+        let target = message.mentions.members.first() || message.guild.members.cache.get(args[0])
         if (!target) {
             target = message.member
         }
