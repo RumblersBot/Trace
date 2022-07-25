09f82d709ef047ddc46f782797ab20d9ffb4f073
MagnoBE <blank@mail.ext>
2022-07-25 11:58:30 +0200

    no longer needed on my user

--- src/specialhandlers/403654158609154058.js
+++ src/specialhandlers/403654158609154058.js
@@ -3,19 +3,19 @@ const RumbleNotification = require('../_database/models/rumbleNotificationSchema
 
 module.exports = {
     run: async ({ message }) => {
-        if (message.mentions.members.size == 0) return
-        const member = await message.guild.members.fetch(message.mentions.members.first())
+        // if (message.mentions.members.size == 0) return
+        // const member = await message.guild.members.fetch(message.mentions.members.first())
 
-        let notifications = await RumbleNotification.find({
-            guildID: message.guild.id,
-            channelID: message.channel.id
-        })
+        // let notifications = await RumbleNotification.find({
+        //     guildID: message.guild.id,
+        //     channelID: message.channel.id
+        // })
 
-        await notifications.forEach(async notif => {
-            if (member.roles.cache.has(notif.roleID)) {
-                const role = await message.guild.roles.fetch(notif.roleID)
-                await message.channel.send(`\`${member.displayName}\` is a member of \`${role.name}\`.`)
-            }
-        })
+        // await notifications.forEach(async notif => {
+        //     if (member.roles.cache.has(notif.roleID)) {
+        //         const role = await message.guild.roles.fetch(notif.roleID)
+        //         await message.channel.send(`\`${member.displayName}\` is a member of \`${role.name}\`.`)
+        //     }
+        // })
     }
 }
