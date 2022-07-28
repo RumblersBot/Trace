1de2d0b60063e06d586ae2e4ed76462f53b1c4a1
MagnoBE <blank@mail.ext>
2022-07-28 10:49:07 +0200

    create embed

---
+++ src/commands/hidden/createembed.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "createembed",
+    category: "hidden",
+    description: 'Create an embed',
+    permissions: 0,
+    usage: "<color>|<title>|<message>",
+    run: async ({ client, message, args }) => {
+        let param = args.join(" ").split("|")
+        let embed = new Discord.MessageEmbed()
+            .setColor(param[0])
+            .setTitle(param[1])
+            .setDescription(param[2])
+
+        embed = client.functions.get("functions").setEmbedFooter(embed, client)
+
+        await message.delete()
+        message.channel.send({
+            embeds: [embed]
+        })
+
+    }
+}
