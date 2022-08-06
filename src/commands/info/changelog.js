94abe64cc45f08f599d3bb965d74aa3522d5fbc3
MagnoBE <blank@mail.ext>
2022-08-06 10:13:23 +0200

    changelog

---
+++ src/commands/info/changelog.js
@@ -0,0 +1,21 @@
+const fs = require("fs")
+const readLastLines = require('read-last-lines');
+
+module.exports = {
+    name: "changelog",
+    category: "info",
+    description: 'Shows the latest ChangeLogs',
+    usage: "[Quantity]",
+    run: async ({message, args}) => {
+        const path = './changelog.md'
+        if (fs.existsSync(path)) {
+            let number = args[0]
+            if (!number) number = 20
+
+            let changelog = await readLastLines.read(path, number)
+            await message.reply({
+                content: `**Last __*${number}*__ changelog Lines:**\n` + changelog
+            })
+        }
+    }
+}
