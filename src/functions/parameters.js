6305bf8a1af08639d06cd6b77db5c4338d3ff5a1
MagnoBE <blank@mail.ext>
2022-08-06 11:27:00 +0200

    different resolves

--- src/functions/parameters.js
+++ src/functions/parameters.js
@@ -4,12 +4,12 @@ async function resolveMember(message, argument, defaultToAuthor) {
 
     if (!!argument) {
 
-        foundMember = message.guild.members.cache.find(entry => entry.user.username === argument)
+        foundMember = message.guild.members.cache.find(entry => entry.user.username.toLowerCase() === argument.toLowerCase())
         if (!!foundMember) return foundMember
 
         foundMember = await message.guild.members.search({ query: argument, limit: 5 })
         if (foundMember.size !== 0) {
-            foundMember = foundMember.filter(entry => entry.user.username === argument)
+            foundMember = foundMember.filter(entry => entry.user.username.toLowerCase() === argument.toLowerCase())
             if (foundMember.size !== 0) return foundMember.first()
         }
 
@@ -25,7 +25,50 @@ async function resolveMember(message, argument, defaultToAuthor) {
     if (defaultToAuthor) return message.member
 }
 
+async function resolveChannel(message, argument, defaultToSourceChannel) {
+    let foundChannel = message.mentions.channels.first()
+    if (foundChannel) return foundChannel
+
+    if (!!argument) {        
+        
+        foundChannel = message.guild.channels.cache.find(entry => entry.name.toLowerCase() === argument.toLowerCase())
+        if (!!foundChannel) return foundChannel
+
+        if (!isNaN(argument)) {
+
+            foundChannel = await message.guild.channels.cache.get(argument)
+            if (!!foundChannel) return foundChannel
+
+            foundChannel = await message.guild.channels.fetch(argument)
+            if (!!foundChannel) return foundChannel
+        }
+    }
+    if (defaultToSourceChannel) return message.channel
+}
+
+async function resolveRole(message, argument) {
+    let foundRole = message.mentions.roles.first()
+    if (foundRole) return foundRole
+
+    if (!!argument) {        
+
+        foundRole = message.guild.roles.cache.find(entry => entry.name.toLowerCase() === argument.toLowerCase())
+        if (!!foundRole) return foundRole
+
+        if (!isNaN(argument)) {
+
+            foundRole = await message.guild.roles.cache.get(argument)
+            if (!!foundRole) return foundRole
+
+            foundRole = await message.guild.roles.fetch(argument)
+            if (!!foundRole) return foundRole
+        }
+    }
+}
+
 module.exports = {
     name: "parameters",
-    resolveMember
+    resolveMember,
+    resolveChannel,
+    resolveRole
 }
