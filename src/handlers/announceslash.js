44f9a6ce57b0887363ebdfb9e56e910c1e54c000
MagnoBE <blank@mail.ext>
2022-08-26 09:39:44 +0200

    Allow slashcommands guild specific

--- src/handlers/announceslash.js
+++ src/handlers/announceslash.js
@@ -5,7 +5,8 @@ module.exports = async (bot) => {
     if (client.slashcommands.size == 0) return
     client.guilds.cache.forEach(guild => {
         console.log(`*** Announcing in [${guild.id}]: [${guild.name}]`)
-        guild.commands.set([...client.slashcommands.values()])
+        const toAnnounce = client.slashcommands.filter(sc => !sc.guilds || sc.guilds.includes(guild.id))
+        guild.commands.set([...toAnnounce.values()])
     });
     console.log("Finished announcing slash command")        
 }
