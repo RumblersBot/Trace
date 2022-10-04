0588fd1f1e6675725f42c2ed4dd2d50717864b3e
MagnoBE <blank@mail.ext>
2022-10-04 16:29:49 +0200

    fix

--- src/commands/battlewinner/listbattlewinner.js
+++ src/commands/battlewinner/listbattlewinner.js
@@ -27,9 +27,9 @@ module.exports = {
 
         await bw.forEach(async entry => {
             const channel = message.guild.channels.cache.get(entry.channelID)
-            const channelName = channel.name || entry.channelID
+            const channelName = channel?.name || entry.channelID
             const role = message.guild.roles.cache.get(entry.roleID)
-            const roleName = role.name || entry.roleID
+            const roleName = role?.name || entry.roleID
             printData.push([`#${channelName}`, `@${roleName}`])
         })
 
